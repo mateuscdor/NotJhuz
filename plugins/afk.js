69d8168408a7810e53b74013cde18a5a3926d7b6
IdkJhus <99780276+IdkJhus@users.noreply.github.com>
2022-03-21 20:38:30 -0600

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text }) => {
   let user = global.DATABASE.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`*❰ ❕ ❱ El usuario ${conn.getName(m.sender)} estará inactivo (AFK) NO lo etiqueten*\n\n*❰ ❕ ❱ Motivo de la inactividad${text ? ': ' + text : ''}*
+  m.reply(`*❰ ❕ ❱ El usuario ${conn.getName(m.sender)} estará inactivo (AFK) NO lo etiqueten*\n\n*❰ ❕ ❱ Motivo de inactividad${text ? ': ' + text : ''}*
 `)
 }
 handler.help = ['afk [alasan]']
